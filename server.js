16828b07daf672bdc23b621dc233a0d06e21138b
Dolsy Smith <dolsy.smith@gmail.com>
2019-01-25 12:24:27 -0500

    Changed port number to avoid conflict

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-server = app.listen(3000);
+server = app.listen(3001);
 
 
 app.post('/', async (req, res) => {
